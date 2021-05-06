cee63d2a784cab73073ece0a395d95a0559bb179
ravisharma-tech <tech.ravisharma191998@gmail.com>
2021-05-06 16:44:04 +0530

    updated code

---
+++ ucertify/src/UI/TopHeader.svelte
@@ -0,0 +1,25 @@
+<scrtip>
+
+</scrtip>
+
+<div class="top__header">
+    <p>www.mymeetups.com</p>
+</div>
+
+<style>
+.top__header {
+    position : fixed;
+    left : 0px;
+    top : 0px;
+    width : 100%;
+    height : 2rem;
+    background-color: purple;
+    text-align : center;
+}
+.top__header p {
+    color : #ffffff;
+    font-size : 1.25rem;
+    margin-top : 0px;
+    font-weight: 100;
+}
+</style>
